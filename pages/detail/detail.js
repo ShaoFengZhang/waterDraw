a29c37fb06440c1a80196dcdca7d806b77cb7a1e
tang <tanglinxuework@163.com>
2018-09-27 12:09:36 +0800

    修改奖品详情文字

--- pages/detail/detail.js
+++ pages/detail/detail.js
@@ -31,7 +31,7 @@ Page({
         toView: '',
         is_new: 1,
         header: {
-            title: '奖品详情',
+            title: '奖品详情2',
             navBack: true
         },
         timeClock: null,
